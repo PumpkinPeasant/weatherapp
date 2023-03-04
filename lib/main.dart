b064ab9125459fcaf01d78c8a38b65589a1e2513
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2023-03-04 17:52:23 +0300

    created home screen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weatherapp/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const GetMaterialApp(
+      home: HomeScreen(),
+      title: 'Погода',
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
-
