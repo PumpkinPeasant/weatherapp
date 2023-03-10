00ed1737c040aa31b3a7cbf22b554e380ce62b99
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2023-03-10 23:25:20 +0300

    get address

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weatherapp/controller/global_controller.dart';
+import 'package:weatherapp/widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,6 +16,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+      child: Obx(() =>
+          globalController.checkLoading().isTrue ? const Center(
+            child: CircularProgressIndicator(),
+          ) : ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              const HeaderWidget()
+            ],
+          )),
+    ));
   }
 }
---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:get/get.dart';
+import 'package:weatherapp/controller/global_controller.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  String city = "";
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
+
+  @override
+  void initState() {
+    getAddress(globalController.getLattitude().value, globalController.getLongitude().value);
+    super.initState();
+  }
+
+  getAddress(lat, lon) async {
+    List<Placemark> placemark = await placemarkFromCoordinates(lat, lon);
+    print(placemark);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Text(city),
+        )
+      ],
+    );
+  }
+}
