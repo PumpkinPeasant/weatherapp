9598ee90c0ca79ea51a08cc2f0f203dc5a3175d7
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2023-03-11 16:21:53 +0300

    request fix

--- lib/api/fetch_weather.dart
+++ lib/api/fetch_weather.dart
@@ -10,7 +10,8 @@ class FetchWeatherAPI {
   WeatherData? weatherData;
 
   Future<WeatherData> processData(lat, lon) async {
-    var response = await http.get(Uri.parse(apiURL(lat, lon)));
+    var response =
+        await http.get(Uri.parse(apiURL(lat, lon)), headers: apiKey());
     var jsonString = jsonDecode(response.body);
     weatherData = WeatherData(WeatherDataCurrent.fromJson(jsonString));
     return weatherData!;
@@ -20,6 +21,13 @@ class FetchWeatherAPI {
 String apiURL(var lat, var lon) {
   String url;
   url =
-      "https://api.weather.yandex.ru/v2/forecast/?lat=$lat&lon=$lon&lang=ru_RU&X-Yandex-API-Key=$apiKey";
+      "https://api.weather.yandex.ru/v2/forecast/?lat=$lat&lon=$lon&lang=ru_RU";
   return url;
+}
+
+Map<String, String> apiKey() {
+  Map<String, String> requestHeaders = {
+    'X-Yandex-API-Key': "82de17c4-ecca-4952-9586-dccf26352a26",
+  };
+  return requestHeaders;
 }
