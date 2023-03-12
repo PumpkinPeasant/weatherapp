f0939c767978f548f77897f09dfc747c6042f247
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2023-03-13 00:16:27 +0300

    daily weather model

---
+++ lib/model/weather_data_daily.dart
@@ -0,0 +1,93 @@
+// class WeatherDataDaily {
+//   List<Hour> daily;
+//
+//   WeatherDataDaily({required this.hourly});
+//
+//   factory WeatherDataDaily.fromJson(Map<String, dynamic> json) {
+//     Forecast forecast = Forecast.fromJson((json['forecasts'] as List<dynamic>)[0]);
+//     return WeatherDataDaily(hourly: forecast.hours);
+//   }
+// }
+
+
+class Forecast {
+  String? date;
+  Parts? parts;
+
+  Forecast({
+    this.date,
+    this.parts,
+  });
+
+  factory Forecast.fromJson(Map<String, dynamic> json) => Forecast(
+    date: json['date'] as String?,
+    parts: json['parts'] == null
+        ? null
+        : Parts.fromJson(json['parts'] as Map<String, dynamic>),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'date': date,
+    'parts': parts?.toJson(),
+  };
+}
+
+class Parts {
+  DayShort? dayShort;
+  NightShort? nightShort;
+
+  Parts({
+    this.dayShort,
+    this.nightShort,
+  });
+
+  factory Parts.fromJson(Map<String, dynamic> json) => Parts(
+        dayShort: json['day_short'] == null
+            ? null
+            : DayShort.fromJson(json['day_short'] as Map<String, dynamic>),
+        nightShort: json['night_short'] == null
+            ? null
+            : NightShort.fromJson(json['night_short'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'day_short': dayShort?.toJson(),
+        'night_short': nightShort?.toJson(),
+      };
+}
+
+class NightShort {
+  int? temp;
+
+  NightShort({
+    this.temp,
+  });
+
+  factory NightShort.fromJson(Map<String, dynamic> json) => NightShort(
+        temp: json['temp'] as int?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'temp': temp,
+      };
+}
+
+class DayShort {
+  int? temp;
+  String? icon;
+
+  DayShort({
+    this.temp,
+    this.icon,
+  });
+
+  factory DayShort.fromJson(Map<String, dynamic> json) => DayShort(
+        temp: json['temp'] as int?,
+        icon: json['icon'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'temp': temp,
+        'icon': icon,
+      };
+}
